5eee58a8af8f56b34729b9c1070566ca60a1d56c
chance <chance.a.cyphers@gmail.com>
2020-03-03 12:48:56 -0500

    add starter re-frame app

---
+++ src/cljs/phlegm/core.cljs
@@ -0,0 +1,23 @@
+(ns phlegm.core
+  (:require
+   [reagent.core :as reagent]
+   [re-frame.core :as re-frame]
+   [phlegm.events :as events]
+   [phlegm.views :as views]
+   [phlegm.config :as config]
+   ))
+
+
+(defn dev-setup []
+  (when config/debug?
+    (println "dev mode")))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (reagent/render [views/main-panel]
+                  (.getElementById js/document "app")))
+
+(defn init []
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (dev-setup)
+  (mount-root))
---
+++ src/cljs/phlegm/db.cljs
@@ -0,0 +1,4 @@
+(ns phlegm.db)
+
+(def default-db
+  {:name "re-frame"})
